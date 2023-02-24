a9dc8ac408f8beb873e831b6d50133f06ade00eb
Titas Gailius <titas.gailius@gmail.com>
2023-02-24 09:41:34 +0000

    Init

---
+++ tests/CalendarTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace TitasGailius\Terminal\Tests;
+
+class CalendarTest extends TestCase
+{
+    //
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TitasGailius\Terminal\Tests;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+abstract class TestCase extends MockeryTestCase
+{
+    //
+}
