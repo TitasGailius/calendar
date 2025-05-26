ebe705bbdb613e70600c938dc16ab7b4a9542a01
Titas Gailius <titas.gailius@gmail.com>
2025-05-26 15:10:07 +0200

    Optional Attendee name

--- src/Resources/Attendee.php
+++ src/Resources/Attendee.php
@@ -9,7 +9,7 @@ class Attendee extends Resource
      */
     public function __construct(
         public string $email,
-        public ?string $name,
+        public ?string $name = null,
         public ?Rsvp $rsvp = null,
     ) {}
 }
